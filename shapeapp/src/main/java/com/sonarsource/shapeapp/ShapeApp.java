83d8d9ad4c1021006c3ca53180f83d4056226897
Manisha Haulbert <manisha.haulbert@sonarsource.com>
2026-01-09 18:59:32 +0100

    testing scm

--- shapeapp/src/main/java/com/sonarsource/shapeapp/ShapeApp.java
+++ shapeapp/src/main/java/com/sonarsource/shapeapp/ShapeApp.java
@@ -7,6 +7,7 @@ import com.sonarsource.shapecore.Circle;
 import com.sonarsource.shapecore.Rectangle;
 import java.util.logging.Logger;
 import java.util.logging.Level;
+import java.util.logging.ConsoleHandler;
 
 
 public class ShapeApp {
